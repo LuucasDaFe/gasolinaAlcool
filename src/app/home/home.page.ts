a29d6533c14366c84a22da9e4318783e9a3dcb85
Lucas Abraao <Labraao957@gmail.com>
2023-07-29 14:29:47 -0300

    App AlcoolGasolina

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public resultado: String = 'resultado';
+  public precoAlcool: string = '';
+  public precoGasolina: string = '';
   constructor() {}
 
+  calcular() {
+    //Validar se os campos fora preenchidos
+    if (this.precoAlcool && this.precoGasolina) {
+      var pAlcool = parseFloat(this.precoAlcool);
+      var pGasolina = parseFloat(this.precoGasolina);
+
+      /*
+        Faz cálculo (precoAlcool / precoGasolina)
+        se: resultado >= 0.7 melhor utilizar gasolina
+        senão: melhor utilizar álcool
+      */
+      var res = pAlcool / pGasolina;
+      if (res >= 0.7) {
+        this.resultado = 'Melhor utilizar Gasolina';
+      } else {
+        this.resultado = 'melhor ultilizar Álcool';
+      }
+    } else {
+      this.resultado = 'Preencha corretamente os campos!';
+    }
+  }
 }
